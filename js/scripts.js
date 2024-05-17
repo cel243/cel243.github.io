46047bc0f46d3b0dcf069237e51c391ba7c83aa9
Caroline Lui <cel243@cornell.edu>
2024-05-16 20:33:42 -0700

    replace website

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,20 @@
-/*!
-* Start Bootstrap - Agency v7.0.10 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+function openDate(evt, dateName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
 
-window.addEventListener('DOMContentLoaded', event => {
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
 
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-});
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(dateName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
---
+++ print_food.py
@@ -0,0 +1,163 @@
+
+import os
+import re
+import sys
+
+OUT_FILE = f"index.html"
+DIRNAME = "FoodView Photos"
+
+def to_am_pm_time(time_str):
+  hr = time_str[:2]
+  m = time_str[3:]
+  txt = "AM"
+  if int(hr) == 12:
+    txt = "PM"
+  if int(hr) > 12:
+    hr = int(hr) - 12
+    txt = "PM"
+  return f"{int(hr)}:{m} {txt}"
+
+def get_header(size, text, styling=""):
+  return f"    <h{size} {styling}>{text}</h{size}>\n"
+
+def get_img_html(src_ls, img_class):
+  img_html_ls = []
+  for src in src_ls:
+    img_html_ls.append(f"      <img class=\"{img_class}\" src=\"{src}\"/>\n")
+  return img_html_ls
+
+def get_time_html(figure_times):
+  if len(figure_times) < 2:
+    return get_header("2", figure_times[0])
+  else:
+    return get_header("2", f"{figure_times[0]} - {figure_times[-1]}")
+
+def get_grouping_id(note):
+  id_search = re.search(".*:([0-9]+):", note)
+  if id_search:
+    return id_search.group(1)
+  else:
+    return None
+
+# Returns the text of the image caption, if it exists, along with
+# other stuff. The full output:
+# new index in filenames (might be the same),
+# Then a tuple that contains:
+# caption, group id, is binge, is note only
+def get_text_caption(filenames, i):
+  if i == len(filenames) - 1:
+    return i, None
+  filename = filenames[i+1]
+  if re.match(".*.jpg$", filename):
+    return i, None
+  else:
+    with open(os.path.join(DIRNAME, filename), 'r') as f:
+      note = f.read().strip()
+      group_id = get_grouping_id(note)
+      is_binge = True if (re.search(".*:binge:", note, re.IGNORECASE) is not None) else None
+      is_note_only = True if (re.search(".*:note:", note, re.IGNORECASE) is not None) else None
+      caption = re.sub(":[^:]+:", "", note).strip()
+      return i+1, (caption, group_id, is_binge, is_note_only)
+
+# return figure_info (possibly updated), new_figure_info (possibly None)
+def get_figure_info(caption_info_tuple, figure_info, img_time, img_source):
+  if not caption_info_tuple:
+    new_figure_info = {
+      "times" : [img_time],
+      "images" : [img_source],
+      "caption_lines" : [],
+      "group_id" : None,
+      "note_only" : False,
+      "binge" : False
+    }
+    return figure_info, new_figure_info
+  if caption_info_tuple:
+    caption, group_id, is_binge, is_note_only = caption_info_tuple
+    if group_id and figure_info and group_id == figure_info["group_id"]:
+      figure_info["times"].append(img_time)
+      figure_info["images"].append(img_source)
+      if caption:
+        figure_info["caption_lines"].append(caption)
+      figure_info["binge"] = figure_info["binge"] or is_binge
+      return figure_info, None
+    else:
+      new_figure_info = {
+        "times" : [img_time],
+        "images" : [img_source],
+        "caption_lines" : [caption] if caption else [],
+        "group_id" : group_id,
+        "note_only" : is_note_only,
+        "binge" : is_binge
+      }
+      return figure_info, new_figure_info
+
+def add_figure_to_output(figure_info, out_lines):
+  if not figure_info:
+    return out_lines
+  binge_class = "binge" if figure_info["binge"] else ""
+  caption = "<br>".join(figure_info["caption_lines"])
+  img_style = "img-class-large" if len(figure_info["images"]) == 1 else "img-class-small"
+  image_html_ls = [""] if figure_info["note_only"] else get_img_html(figure_info["images"], img_style)
+  return out_lines + (
+    [f"    <figure class=\"outer-figure\"><figure class=\"inner-figure {binge_class}\">\n",
+            get_time_html(figure_info["times"])] +
+            image_html_ls + [
+    f"      </figure>",
+    f"      <figcaption>{caption}</figcaption>\n",
+     "    </figure>\n"])
+
+
+out_lines = []
+nav_tabs = "<div class=\"tab\">"
+
+filenames = os.listdir(DIRNAME)
+filenames.sort() 
+date = None
+image_id = None
+figure_info = None
+i = 0
+while i < len(filenames):
+  filename = filenames[i]
+  if not re.search("^\d{4}", filename):
+    i += 1
+    continue
+  # if DATE is not None:
+  #   if not re.search(f"^{DATE}", filename):
+  #     i += 1
+  #     continue
+
+  if re.match(".*.jpg$", filename):
+    img_time = to_am_pm_time(re.search("T(\d{2}-\d{2})", filename).group(1).replace("-", ":"))
+    img_source = f"{DIRNAME}/{filename}"
+    i, caption_info_tuple = get_text_caption(filenames, i)
+    figure_info, new_figure_info = get_figure_info(caption_info_tuple, figure_info, img_time, img_source)
+    if new_figure_info:
+      # Should print the old figure info if we found a new figure!
+      out_lines = add_figure_to_output(figure_info, out_lines)
+      figure_info = new_figure_info
+
+  date_from_filename = re.search("^(\d{4}-\d{2}-\d{2})", filename).group(1)
+  if date_from_filename != date:
+    date = date_from_filename
+    out_lines.append(f"    </div><div id=\"{date}\"><h1>{date}</h1>\n")
+    nav_tabs += f"<button class=\"tablinks\" onclick=\"openDate(event, '{date}')\">{date}</button>"
+
+  i += 1
+
+out_lines = add_figure_to_output(figure_info, out_lines)
+
+out_lines = [
+"<!doctype html>\n",
+"<html lang=\"en\">\n",
+"  <head>\n",
+"    <link href=\"css/styles.css\" rel=\"stylesheet\" />\n",
+"  <body> \n",
+f"{nav_tabs}</div>\n",
+"  <div>\n"
+] + out_lines + [
+  "  <script src=\"js/scripts.js\"></script>\n",
+  "  </body>\n",
+"</html>\n"
+]
+with open(OUT_FILE, 'w') as f:
+  f.writelines(out_lines)
